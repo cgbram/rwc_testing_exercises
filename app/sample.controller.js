7c5b1968d7ec66a510e9a1dde3f51069640e0602
Tomasz Szewcow <tomasz.szewcow@capgemini.com>
2016-03-27 18:25:18 +0200

    unnecessary code deleted

--- app/sample.controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('app').controller('SampleController', function($scope, $location){
-    'use strict';
-
-    $scope.goToDialog = function(path){
-        $location.path(path);
-    };
-});
