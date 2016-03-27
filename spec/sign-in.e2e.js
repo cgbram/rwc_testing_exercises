30c6402fa0bc50349c27574ecf49837659beca82
Tomasz Szewcow <tomasz.szewcow@capgemini.com>
2016-03-27 18:57:54 +0200

    code adjusted

--- spec/sign-in.e2e.js
+++ spec/sign-in.e2e.js
@@ -8,16 +8,12 @@ describe('sign in page', function () {
         signInPage = new SignInPage();
     });
 
-    afterEach(function(){
-       signInPage.signOut();
-    });
-
     it('should open sign in page, enter credentials and sign in', function() {
 
-        signInPage.open();
+        signInPage.open('http://oasp-ci.cloudapp.net/oasp4j-sample/jsclient/#/main/sign-in');
         signInPage.enterUserName('waiter');
         signInPage.enterPassword('waiter');
-        signInPage.clickSignInButton();
+        signInPage.signIn();
 
         browser.wait(function() {
             return browser.getCurrentUrl().then(function(url) {
